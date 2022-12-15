ad3337c25712296226fa6a78472e9dd487e88624
ayowandeapp <ayooluwa71@gmail.com>
2022-12-15 22:01:54 +0100

    Revert "Revert "initial commit""
    This reverts commit de58e0d258e0a502f9c0b033d637046fb1f9be70.

---
+++ resources/js/youtube/VideoItem.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="col-sm-6">
+		<div class="card" style="width:400px">
+		  <img class="card-img-top" :src="videoImage" :alt="videoTitle"width="200px">
+		  <div class="card-body">
+		    <h4 class="card-title">{{videoTitle}}</h4>
+		    <p class="card-text">{{videoDescription}}</p>
+		    <a href="#" class="btn btn-primary">See Profile</a>
+		  </div>
+		</div>
+	</div>
+</template>
+<script>
+    export default {
+    	props:['video'],
+    	computed: {
+    		videoTitle() {
+    			return this.video.snippet.title;
+    		},
+    		videoDescription() {
+    			return this.video.snippet.description;
+    		},
+    		videoImage(){
+    			return this.video.snippet.thumbnails.medium.url; 
+    		}
+
+    	}
+}
+</script>
