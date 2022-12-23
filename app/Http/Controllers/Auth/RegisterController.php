4f62b64cc51605444bf132b5f07bbecf6b19c5d4
ayowandeapp <ayooluwa71@gmail.com>
2022-12-23 11:46:48 +0100

    pusher resources, and login resources

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,12 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Services\AccountActivation;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Registered;
+use App\Mail\ActivateUserAccount;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -28,7 +34,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -67,6 +73,21 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'is_active' => 0,
+            'activation_token' => AccountActivation::generateToken(),
         ]);
     }
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        $user = $this->create($request->all());
+
+        event(new Registered($user));
+
+        Mail::to($user)->send(new ActivateUserAccount($user));
+
+        return $this->registered($request, $user)
+                        ?: redirect($this->redirectPath());
+    }
+    
 }
